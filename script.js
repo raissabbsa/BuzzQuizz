a81104d4aefa8d6b425c4e3bd75b5f2977682103
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-09-02 11:32:41 -0300

    tela 2 renderizar respostas quizz aleatoriamente

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 // resolucao tela 2
 
+function comparador() {
+  return Math.random() -0.5;
+}
+let ordemRespostas = [0, 1, 2 ,3];
+
 function mostrarTela2() {
     const ocultarTela1 = document.querySelector("tela1");
     ocultarTela1.classList.add(esconder);
@@ -12,7 +17,7 @@ function mostrarTela2() {
 
 function buscarQuizzEscolhido() {
     
-    const pegarQuizz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/2");
+    const pegarQuizz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/1");
     pegarQuizz.then(renderizarQuizzEscolhido);
     console.log(pegarQuizz);
 }
@@ -35,6 +40,16 @@ function renderizarQuizzEscolhido(dados) {
 
        for (let i = 0; i < numPerguntas; i++) {
         
+        ordemRespostas.sort(comparador);
+        let k = ordemRespostas[0];
+        let j = ordemRespostas[1];
+        let s = ordemRespostas[2];
+        let t = ordemRespostas[3];
+        console.log(k);
+        console.log(j);
+        console.log(s);
+        console.log(t);
+        console.log(ordemRespostas);
 
         renderizarPerguntas.innerHTML = renderizarPerguntas.innerHTML + `<div class="caixa-perguntas">
         <div class="pergunta">
@@ -42,48 +57,52 @@ function renderizarQuizzEscolhido(dados) {
         </div>
         <div class="caixa-respostas">
           <div class="caixa-respostas-esquerda">
-            <div class="resposta-figura"> 
+            <div class="resposta-figura" onclick="selecionarResposta(this);"> 
               <div class="figura">
-                <img src="${dados.data.questions[i].answers[0].image}" />
+                <img src="${dados.data.questions[i].answers[k].image}" />
               </div>
               <div class="resposta">
-                <h1 class="resposta-texto">${dados.data.questions[i].answers[0].text}</h1>
+                <h1 class="resposta-texto">${dados.data.questions[i].answers[k].text}</h1>
               </div>
             </div>
-            <div class="resposta-figura">
+            <div class="resposta-figura" onclick="selecionarResposta(this);">
               <div class="figura">
-                <img src="${dados.data.questions[i].answers[1].image}" />
+                <img src="${dados.data.questions[i].answers[j].image}" />
               </div>
               <div class="resposta">
-                <h1 class="resposta-texto">${dados.data.questions[i].answers[1].text}</h1>
+                <h1 class="resposta-texto">${dados.data.questions[i].answers[j].text}</h1>
               </div>
             </div>
           </div>
           <div class="caixa-respostas-direita">
-            <div class="resposta-figura">
+            <div class="resposta-figura" onclick="selecionarResposta(this);">
               <div class="figura">
-                <img src="${dados.data.questions[i].answers[2].image}" />
+                <img src="${dados.data.questions[i].answers[s].image}" />
               </div>
               <div class="resposta">
-                <h1 class="resposta-texto">${dados.data.questions[i].answers[2].text}</h1>
+                <h1 class="resposta-texto">${dados.data.questions[i].answers[s].text}</h1>
               </div>
             </div>
-            <div class="resposta-figura">
+            <div class="resposta-figura" onclick="selecionarResposta(this);">
               <div class="figura">
-                <img src="${dados.data.questions[i].answers[3].image}" />
+                <img src="${dados.data.questions[i].answers[t].image}" />
               </div>
               <div class="resposta">
-                <h1 class="resposta-texto">${dados.data.questions[i].answers[3].text}</h1>
+                <h1 class="resposta-texto">${dados.data.questions[i].answers[t].text}</h1>
               </div>
             </div>
           </div>
         </div>
       </div>`
     }  
-
 }
 
 buscarQuizzEscolhido(); 
+
+function selecionarResposta(resposta) {
+  console.log("sim");
+  console.log(resposta);
+}
  
 const pegaQuiz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
 console.log(pegaQuiz);  
