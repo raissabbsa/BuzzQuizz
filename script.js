b6219ded741fddfaea199849d310db287c9fcb97
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-09-01 23:37:43 -0300

    versao tela 2 renderizando quizzes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+// resolucao tela 2
+
 function mostrarTela2() {
     const ocultarTela1 = document.querySelector("tela1");
     ocultarTela1.classList.add(esconder);
@@ -8,15 +10,80 @@ function mostrarTela2() {
     renderizarQuizzEscolhido()
 }
 
-function renderizarQuizzEscolhido() {
+function buscarQuizzEscolhido() {
     
-    const pegarQuizz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/1");
-    pegarQuizz.then(trazerResposta);
+    const pegarQuizz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/2");
+    pegarQuizz.then(renderizarQuizzEscolhido);
     console.log(pegarQuizz);
 }
 
-function trazerResposta() {
-    console.log("chegou")
+function renderizarTituloQuizz(titulo, imagem) {
+    const tituloQuizz = document.querySelector(".titulo-quizz-tela2");
+    tituloQuizz.innerHTML = `<img src="${imagem}" />
+                                <h1>${titulo}</h1>`
+    
 }
 
-renderizarQuizzEscolhido();
+function renderizarQuizzEscolhido(dados) {
+    console.log("chegou");
+    let numPerguntas = dados.data.questions.length;
+    console.log(dados.data.title);
+    renderizarTituloQuizz(dados.data.title, dados.data.image);
+    console.log(dados.data.questions[0].answers[0].image)
+    const renderizarPerguntas =  document.querySelector(".caixaquizz-tela2");
+    renderizarPerguntas.innerHTML = "";
+
+       for (let i = 0; i < numPerguntas; i++) {
+        
+
+        renderizarPerguntas.innerHTML = renderizarPerguntas.innerHTML + `<div class="caixa-perguntas">
+        <div class="pergunta">
+          <h1>${dados.data.questions[i].title}</h1>
+        </div>
+        <div class="caixa-respostas">
+          <div class="caixa-respostas-esquerda">
+            <div class="resposta-figura"> 
+              <div class="figura">
+                <img src="${dados.data.questions[i].answers[0].image}" />
+              </div>
+              <div class="resposta">
+                <h1 class="resposta-texto">${dados.data.questions[i].answers[0].text}</h1>
+              </div>
+            </div>
+            <div class="resposta-figura">
+              <div class="figura">
+                <img src="${dados.data.questions[i].answers[1].image}" />
+              </div>
+              <div class="resposta">
+                <h1 class="resposta-texto">${dados.data.questions[i].answers[1].text}</h1>
+              </div>
+            </div>
+          </div>
+          <div class="caixa-respostas-direita">
+            <div class="resposta-figura">
+              <div class="figura">
+                <img src="${dados.data.questions[i].answers[2].image}" />
+              </div>
+              <div class="resposta">
+                <h1 class="resposta-texto">${dados.data.questions[i].answers[2].text}</h1>
+              </div>
+            </div>
+            <div class="resposta-figura">
+              <div class="figura">
+                <img src="${dados.data.questions[i].answers[3].image}" />
+              </div>
+              <div class="resposta">
+                <h1 class="resposta-texto">${dados.data.questions[i].answers[3].text}</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+    }  
+
+}
+
+buscarQuizzEscolhido(); 
+ 
+const pegaQuiz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+console.log(pegaQuiz);  
